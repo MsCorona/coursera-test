5a1c225ec1b689d0f6733e394bfd30f05795e148
Tara Corona <taracorona@gmail.com>
2022-04-17 11:26:34 -0500

    week4 solution

---
+++ site/week4/script.js
@@ -0,0 +1,30 @@
+var helloSpeaker = new Object();
+helloSpeaker.speak = function(name) {
+  console.log('Hello '+name);
+}
+
+var byeSpeaker = new Object();
+byeSpeaker.speak = function(name) {
+  console.log('Goodbye '+name);
+}
+
+
+
+function sayHello(name) {
+  console.log('hello '+name);
+}
+
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+for(var i = 0; i < names.length; i++) {     
+  
+  var firstLetter = names[i].charAt(0).toLowerCase(); 
+  
+  if(firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {  
+    helloSpeaker.speak(names[i]);
+  }  
+}
